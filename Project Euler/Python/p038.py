707a6141ed35f9c38a8637fd3386ae4338799d22
marik348 <vadimich348@gmail.com>
2021-01-21 08:43:04 +0200

    Add solution for 38th problem

---
+++ Project Euler/Python/p038.py
@@ -0,0 +1,12 @@
+def is_pandigital(num):
+	return "".join(sorted(str(num))) == '123456789'
+
+answer = "1"
+for i in range(9,9999):
+
+	temp = "".join([str(i*n) for n in range(1,6)])
+
+	if is_pandigital(temp[:9]):
+		answer = max(answer, temp)
+
+print(answer[:9])
