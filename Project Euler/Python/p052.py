2d154e6ff8e8e0a4ee749dbb1ce483f54c2ee7b2
marik348 <vadimich348@gmail.com>
2021-01-30 11:37:30 +0200

    Add solution for 52th problem

---
+++ Project Euler/Python/p052.py
@@ -0,0 +1,12 @@
+def has_same_digits(a,b):
+	return sorted(str(a)) == sorted(str(b))
+
+x = 1
+while True:
+	if all(has_same_digits(x, x*i) for i in [2,3,4,5,6]):
+		answer = x
+		break
+
+	x += 1
+
+print(answer)
