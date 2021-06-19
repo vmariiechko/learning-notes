6d572c47db6f3b3e87814a01157f7b27e1d56454
Vadym Mariiechko <vadimich348@gmail.com>
2021-06-19 17:39:04 +0200

    Add checkers game logic

--- Projects/C#/Checkers TCP/Client/Client/Board.cs
+++ Projects/C#/Checkers TCP/Client/Client/Board.cs
@@ -12,19 +12,25 @@ namespace Client
 {
     public partial class Board : Form
     {
-        private const int mapSize = 8;
-        private const int cellSize = 50;
+        const int mapSize = 8;
+        const int cellSize = 50;
 
-        private int currentPlayer;
+        int currentPlayer;
 
+        List<Button> simpleSteps = new List<Button>();
+
+        int countEatSteps = 0;
         Button prevButton;
+        Button pressedButton;
+        bool isContinue = false;
 
         bool isMoving;
 
-        private Image whiteFigure;
-        private Image blackFigure;
-
         int[,] map = new int[mapSize, mapSize];
+        Button[,] buttons = new Button[mapSize, mapSize];
+
+        Image whiteFigure;
+        Image blackFigure;
         public Board()
         {
             InitializeComponent();
@@ -55,22 +61,47 @@ namespace Client
             CreateMap();
         }
 
+        public void ResetGame()
+        {
+            bool player1 = false;
+            bool player2 = false;
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (map[i, j] == 1) player1 = true;
+                    if (map[i, j] == 2) player2 = true;
+                }
+            }
+            if (!player1 || !player2)
+            {
+                this.Controls.Clear();
+                Init();
+            }
+        }
+
         public void CreateMap()
         {
             this.Width = (mapSize + 1) * cellSize;
             this.Height = (mapSize + 1) * cellSize;
 
-            for (int i = 0; i < mapSize; ++i)
+            for (int i = 0; i < mapSize; i++)
             {
-                for (int j = 0; j < mapSize; ++j)
+                for (int j = 0; j < mapSize; j++)
                 {
                     Button button = new Button();
                     button.Location = new Point(j * cellSize, i * cellSize);
                     button.Size = new Size(cellSize, cellSize);
                     button.Click += new EventHandler(OnFigurePress);
-                    if (map[i, j] == 1) button.Image = whiteFigure;
+                    if (map[i, j] == 1)
+                        button.Image = whiteFigure;
                     else if (map[i, j] == 2) button.Image = blackFigure;
                     button.BackColor = GetPrevButtonColor(button);
+                    button.ForeColor = Color.Red;
+
+                    buttons[i, j] = button;
+
                     this.Controls.Add(button);
                 }
             }
@@ -78,6 +109,7 @@ namespace Client
         public void SwitchPlayer()
         {
             currentPlayer = currentPlayer == 1 ? 2 : 1;
+            ResetGame();
         }
 
         public Color GetPrevButtonColor(Button prevButton)
@@ -101,29 +133,453 @@ namespace Client
 
         public void OnFigurePress(object sender, EventArgs e)
         {
-            if (prevButton != null) prevButton.BackColor = GetPrevButtonColor(prevButton);
-            Button pressedButton = sender as Button;
+
+            if (prevButton != null)
+                prevButton.BackColor = GetPrevButtonColor(prevButton);
+
+            pressedButton = sender as Button;
 
             if (map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] != 0 && map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] == currentPlayer)
             {
+                CloseSteps();
                 pressedButton.BackColor = Color.Red;
-                isMoving = true;
+                DeactivateAllButtons();
+                pressedButton.Enabled = true;
+                countEatSteps = 0;
+                if (pressedButton.Text == "D")
+                    ShowSteps(pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize, false);
+                else ShowSteps(pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize);
+
+                if (isMoving)
+                {
+                    CloseSteps();
+                    pressedButton.BackColor = GetPrevButtonColor(pressedButton);
+                    ShowPossibleSteps();
+                    isMoving = false;
+                }
+                else
+                    isMoving = true;
             }
             else
             {
                 if (isMoving)
                 {
+                    isContinue = false;
+                    if (Math.Abs(pressedButton.Location.X / cellSize - prevButton.Location.X / cellSize) > 1)
+                    {
+                        isContinue = true;
+                        DeleteEaten(pressedButton, prevButton);
+                    }
                     int temp = map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize];
                     map[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = map[prevButton.Location.Y / cellSize, prevButton.Location.X / cellSize];
                     map[prevButton.Location.Y / cellSize, prevButton.Location.X / cellSize] = temp;
                     pressedButton.Image = prevButton.Image;
                     prevButton.Image = null;
+                    pressedButton.Text = prevButton.Text;
+                    prevButton.Text = "";
+                    SwitchButtonToCheat(pressedButton);
+                    countEatSteps = 0;
                     isMoving = false;
-                    SwitchPlayer();
+                    CloseSteps();
+                    DeactivateAllButtons();
+                    if (pressedButton.Text == "D")
+                        ShowSteps(pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize, false);
+                    else ShowSteps(pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize);
+                    if (countEatSteps == 0 || !isContinue)
+                    {
+                        CloseSteps();
+                        SwitchPlayer();
+                        ShowPossibleSteps();
+                        isContinue = false;
+                    }
+                    else if (isContinue)
+                    {
+                        pressedButton.BackColor = Color.Red;
+                        pressedButton.Enabled = true;
+                        isMoving = true;
+                    }
                 }
             }
 
             prevButton = pressedButton;
         }
+
+        public void ShowPossibleSteps()
+        {
+            bool isOneStep = true;
+            bool isEatStep = false;
+            DeactivateAllButtons();
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    if (map[i, j] == currentPlayer)
+                    {
+                        if (buttons[i, j].Text == "D") isOneStep = false;
+                        else isOneStep = true;
+                        if (IsButtonHasEatStep(i, j, isOneStep, new int[2] { 0, 0 }))
+                        {
+                            isEatStep = true;
+                            buttons[i, j].Enabled = true;
+                        }
+                    }
+                }
+            }
+            if (!isEatStep) ActivateAllButtons();
+        }
+
+        public void SwitchButtonToCheat(Button button)
+        {
+            if (map[button.Location.Y / cellSize, button.Location.X / cellSize] == 1 && button.Location.Y / cellSize == mapSize - 1)
+            {
+                button.Text = "D";
+
+            }
+            if (map[button.Location.Y / cellSize, button.Location.X / cellSize] == 2 && button.Location.Y / cellSize == 0)
+            {
+                button.Text = "D";
+            }
+        }
+
+        public void DeleteEaten(Button endButton, Button startButton)
+        {
+            int count = Math.Abs(endButton.Location.Y / cellSize - startButton.Location.Y / cellSize);
+            int startIndexX = endButton.Location.Y / cellSize - startButton.Location.Y / cellSize;
+            int startIndexY = endButton.Location.X / cellSize - startButton.Location.X / cellSize;
+            startIndexX = startIndexX < 0 ? -1 : 1;
+            startIndexY = startIndexY < 0 ? -1 : 1;
+            int currCount = 0;
+            int i = startButton.Location.Y / cellSize + startIndexX;
+            int j = startButton.Location.X / cellSize + startIndexY;
+            while (currCount < count - 1)
+            {
+                map[i, j] = 0;
+                buttons[i, j].Image = null;
+                buttons[i, j].Text = "";
+                i += startIndexX;
+                j += startIndexY;
+                currCount++;
+            }
+
+        }
+
+        public void ShowSteps(int iCurrFigure, int jCurrFigure, bool isOnestep = true)
+        {
+            simpleSteps.Clear();
+            ShowDiagonal(iCurrFigure, jCurrFigure, isOnestep);
+            if (countEatSteps > 0) CloseSimpleSteps(simpleSteps);
+        }
+
+        public void ShowDiagonal(int IcurrFigure, int JcurrFigure, bool isOneStep = false)
+        {
+            int j = JcurrFigure + 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (currentPlayer == 1 && isOneStep && !isContinue) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (currentPlayer == 1 && isOneStep && !isContinue) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (currentPlayer == 2 && isOneStep && !isContinue) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure + 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (currentPlayer == 2 && isOneStep && !isContinue) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (!DeterminePath(i, j))
+                        break;
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+        }
+
+        public bool DeterminePath(int ti, int tj)
+        {
+
+            if (map[ti, tj] == 0 && !isContinue)
+            {
+                buttons[ti, tj].BackColor = Color.Yellow;
+                buttons[ti, tj].Enabled = true;
+                simpleSteps.Add(buttons[ti, tj]);
+            }
+            else
+            {
+
+                if (map[ti, tj] != currentPlayer)
+                {
+                    if (pressedButton.Text == "D")
+                        ShowProceduralEat(ti, tj, false);
+                    else ShowProceduralEat(ti, tj);
+                }
+
+                return false;
+            }
+            return true;
+        }
+
+        public void CloseSimpleSteps(List<Button> simpleSteps)
+        {
+            if (simpleSteps.Count > 0)
+            {
+                for (int i = 0; i < simpleSteps.Count; i++)
+                {
+                    simpleSteps[i].BackColor = GetPrevButtonColor(simpleSteps[i]);
+                    simpleSteps[i].Enabled = false;
+                }
+            }
+        }
+
+        public void ShowProceduralEat(int i, int j, bool isOneStep = true)
+        {
+            int dirX = i - pressedButton.Location.Y / cellSize;
+            int dirY = j - pressedButton.Location.X / cellSize;
+            dirX = dirX < 0 ? -1 : 1;
+            dirY = dirY < 0 ? -1 : 1;
+            int il = i;
+            int jl = j;
+            bool isEmpty = true;
+            while (IsInsideBorders(il, jl))
+            {
+                if (map[il, jl] != 0 && map[il, jl] != currentPlayer)
+                {
+                    isEmpty = false;
+                    break;
+                }
+                il += dirX;
+                jl += dirY;
+
+                if (isOneStep)
+                    break;
+            }
+            if (isEmpty)
+                return;
+            List<Button> toClose = new List<Button>();
+            bool closeSimple = false;
+            int ik = il + dirX;
+            int jk = jl + dirY;
+            while (IsInsideBorders(ik, jk))
+            {
+                if (map[ik, jk] == 0)
+                {
+                    if (IsButtonHasEatStep(ik, jk, isOneStep, new int[2] { dirX, dirY }))
+                    {
+                        closeSimple = true;
+                    }
+                    else
+                    {
+                        toClose.Add(buttons[ik, jk]);
+                    }
+                    buttons[ik, jk].BackColor = Color.Yellow;
+                    buttons[ik, jk].Enabled = true;
+                    countEatSteps++;
+                }
+                else break;
+                if (isOneStep)
+                    break;
+                jk += dirY;
+                ik += dirX;
+            }
+            if (closeSimple && toClose.Count > 0)
+            {
+                CloseSimpleSteps(toClose);
+            }
+
+        }
+
+        public bool IsButtonHasEatStep(int IcurrFigure, int JcurrFigure, bool isOneStep, int[] dir)
+        {
+            bool eatStep = false;
+            int j = JcurrFigure + 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (currentPlayer == 1 && isOneStep && !isContinue) break;
+                if (dir[0] == 1 && dir[1] == -1 && !isOneStep) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i - 1, j + 1))
+                            eatStep = false;
+                        else if (map[i - 1, j + 1] != 0)
+                            eatStep = false;
+                        else return eatStep;
+                    }
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure - 1; i >= 0; i--)
+            {
+                if (currentPlayer == 1 && isOneStep && !isContinue) break;
+                if (dir[0] == 1 && dir[1] == 1 && !isOneStep) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i - 1, j - 1))
+                            eatStep = false;
+                        else if (map[i - 1, j - 1] != 0)
+                            eatStep = false;
+                        else return eatStep;
+                    }
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure - 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (currentPlayer == 2 && isOneStep && !isContinue) break;
+                if (dir[0] == -1 && dir[1] == 1 && !isOneStep) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i + 1, j - 1))
+                            eatStep = false;
+                        else if (map[i + 1, j - 1] != 0)
+                            eatStep = false;
+                        else return eatStep;
+                    }
+                }
+                if (j > 0)
+                    j--;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+
+            j = JcurrFigure + 1;
+            for (int i = IcurrFigure + 1; i < 8; i++)
+            {
+                if (currentPlayer == 2 && isOneStep && !isContinue) break;
+                if (dir[0] == -1 && dir[1] == -1 && !isOneStep) break;
+                if (IsInsideBorders(i, j))
+                {
+                    if (map[i, j] != 0 && map[i, j] != currentPlayer)
+                    {
+                        eatStep = true;
+                        if (!IsInsideBorders(i + 1, j + 1))
+                            eatStep = false;
+                        else if (map[i + 1, j + 1] != 0)
+                            eatStep = false;
+                        else return eatStep;
+                    }
+                }
+                if (j < 7)
+                    j++;
+                else break;
+
+                if (isOneStep)
+                    break;
+            }
+            return eatStep;
+        }
+
+        public bool IsInsideBorders(int ti, int tj)
+        {
+            if (ti >= mapSize || tj >= mapSize || ti < 0 || tj < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void CloseSteps()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    buttons[i, j].BackColor = GetPrevButtonColor(buttons[i, j]);
+                }
+            }
+        }
+
+        public void ActivateAllButtons()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+
+        public void DeactivateAllButtons()
+        {
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    buttons[i, j].Enabled = false;
+                }
+            }
+        }
+
     }
 }
